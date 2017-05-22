2cf53ecdee9fe061769293b9f78ac274f15c9471
tlyong1992 <tlyong1992@hotmail.com>
2017-05-22 16:48:35 +0800

    枚举类增加文字描述

--- src/main/java/com/tlyong1992/client/constant/Direction.java
+++ src/main/java/com/tlyong1992/client/constant/Direction.java
@@ -6,5 +6,33 @@ package com.tlyong1992.client.constant;
  * TIME：14:04
  */
 public enum Direction {
-    L, LU, U, RU, R, RD, D, LD, STOP
+
+    L("左"),
+
+    LU("左上"),
+
+    U("上"),
+
+    RU("右上"),
+
+    R("右"),
+
+
+    RD("右下"),
+
+    D("下"),
+
+    LD("左下"),
+
+    STOP("停止");
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    Direction(String value) {
+        this.value = value;
+    }
 }
